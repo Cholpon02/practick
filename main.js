9da8f48e7ff80275b234e37758b56b4cca08c942
Wolfer <djumanalievacholponpi220@gmail.com>
2023-02-22 11:46:07 +0600

    init

---
+++ main.js
@@ -0,0 +1,45 @@
+* methods
+* getElementsByTagName()
+* getElementsByClassName()
+* getElementsById()
+* InnerHTML  innerText
+
+// document.getElementsByClassName('h1')[0].innerText="Hello world!"
+// document.getElementsByTagName("h1")[0].innerText="Privet Mir"
+// document.getElementById("h1")[2].innerText="Привет мир!"
+
+// let title1=document.getElementById("title1")
+// let title2=document.getElementById("title2")
+// let title3=document.getElementById("title3")
+// let title4=document.getElementById("title4")
+
+// let h1=document.getElementsByTagName("h1")
+// title1.style.color = "red"
+// title2.style.color = "green"
+// title2.style.color = "yellow"
+// title2.style.color = "blue"
+// h1[0].innerHTML="Hello world!"
+// h1[1].innerHTML="Hello world!"
+// h1[2].innerHTML="Hello world!"
+// h1[3].innerHTML="Hello world!"
+
+// document.body.innerHTML='<h1>Hello world!</h1>'
+
+// let text=document.getElementsByTagName("h1") [0]
+
+// let text=document.getElementsByTagName("h1")[0]
+// let colors=["red", "green", "orange", "blue", "rainbow", "silver"]
+//  text.innerText="Hello world!";
+
+// let i = 0
+// setInterval(()=> {
+//     text.style.color = [i]
+//     if (i === 6) {
+//         i = 0
+//     } else {
+//         i++
+//     }
+// }, 500)
+
+
+
